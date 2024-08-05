d8fa617c3a71b3f7d5d5cf689c382de213c21b09
pyro <62857754+calebskinner@users.noreply.github.com>
2024-08-04 23:04:43 -0400

    remove .zip

---
+++ ff_antibump/fxmanifest.lua
@@ -0,0 +1,16 @@
+------------------------------------------------------------------------------------
+-- FiveForge Anti Bump
+-- Configured By akaLucifer. & pyrs
+-- Releasing or Claiming this as your own is against, this resources License
+------------------------------------------------------------------------------------
+fx_version "adamant"
+game "gta5"
+
+lua54 "yes"
+
+files {
+	"data/handlings/.meta",
+}
+
+-- Vehicles
+data_file "HANDLING_FILE" "data/handlings/*.meta"
